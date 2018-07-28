078f99cb82c1b9b9f9192f284db1f9ddae8b1da8
Imen Mahjoubi <imen.mahjoubi@esprit.tn>
2018-07-28 13:57:27 -0700

    small fix

--- userPromt.js
+++ userPromt.js
@@ -1,16 +1,5 @@
 // Create a basic command line Node application using the inquirer package.
 var inquirer = require('inquirer');
-
-// Your application should ask the user any five questions of your choosing.
-// The question set should include at least one:
-
-//    - Basic input,
-//    - Password,
-//    - List,
-//    - Checkbox,
-//    - and Confirm
-
-
 inquirer
     .prompt([
         // Here we create a basic text prompt.
@@ -87,9 +76,3 @@ inquirer
 
 });
 
-// Then if a user's password matches a pre-defined password, re-display the data back to the user with some text.
-// See the inquirer GitHub documentation "examples" page if you need help.
-
-// Remember to be creative!
-
-// ========================================================================
